41f8a0a1bc61d6c33218002ab366c9f1c12ea757
jerrita <782981223@qq.com>
2021-06-22 17:57:04 +0800

    新增微码提示
    实体机记得装微码

--- install.sh
+++ install.sh
@@ -80,6 +80,7 @@ useradd -mG wheel $username
 passwd $username
 
 echo "Now you can modify by yourself and reboot."
+echo "If you install it on real machine, remember install intel_ucode or amd_ucode"
 EOF
 echo "Now you can goto /root and bash nextstep."
 arch-chroot /mnt
