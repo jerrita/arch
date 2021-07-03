f2230b6edce391ca1dcc40e1f64e45eb913d52de
jerrita <782981223@qq.com>
2021-07-03 11:19:21 +0800

    update aur

--- utils/aur_helper.sh
+++ utils/aur_helper.sh
@@ -1,13 +1,21 @@
+if whoami -eq "root"
+then
+  echo "Please use non-root user."
+  exit
+fi
+
 if cat /etc/pacman.conf | grep "archlinuxcn" > /dev/null
 then
 echo "Aur repo exists. Exit."
 else
 cat<<EOF>>/etc/pacman.conf
 [archlinuxcn]
-Server = https://mirrors.cqupt.edu.cn/archlinuxcn
+Server = https://mirrors.cqupt.edu.cn/archlinuxcn/$arch
 EOF
 pacman -Syy
 fi
 
-git clone https://aur.archlinux.org/yay.git /opt/yay
+sudo pacman -S fakeroot
+sudo git clone https://aur.archlinux.org/yay.git /opt/yay
+sudo chown -R ${whoami}:users /opt/yay
 cd /opt/yay && makepkg -si
